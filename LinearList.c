7b0d480e050f49e3961a42d948d64803ee3b6a7f
孙波 <16491542+sunbo-060214@user.noreply.gitee.com>
2026-05-14 18:01:34 +0800

    完成线性表基础操作实现

---
+++ LinearList.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#define MAX_SIZE 100
+
+typedef int ElemType;
+
+
+//顺序表
+typedef struct {
+	ElemType data[MAX_SIZE];
+	int length;
+}SeqList;
+
+void initList(SeqList * L) {
+	L -> length = 0;
+}//初始化顺序表
+
+
+
+int main() {
+
+
+	//顺序表
+	//声明一个顺序表并初始化
+	SeqList list;
+	initList(&list);
+	printf("顺序表初始化完成，当前长度为: %d\n", list.length);
+	printf("目前占用的存储空间为: %d\n", sizeof(list.data));
+
+
+
+
+
+
+
+	return 0;
+}
